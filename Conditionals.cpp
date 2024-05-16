62ceb0ad22ff15b4117cdc3eac42af03bb21dfcf
David-Szczepanik <Davidszczepanik@seznam.cz>
2024-05-16 18:06:33 +0200

    Conditionals

---
+++ Conditionals.cpp
@@ -0,0 +1,9 @@
+#include "Log.h"
+
+void Conditionals() {
+  int x = 5;
+  bool comparisonResult = x == 5;
+  if (comparisonResult) {
+    Log("Comparison is true");
+  }
+}
--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
-#include <iostream>
-
 void Log(const char* message);
 void Variables();
+void Conditionals();
 
 int main() {
-  Log("Hello World!");
-  Variables();
+  // Log("Hello World!");
+  // Variables();
+  Conditionals();
+
+
+
   // std::cin.get();
   return 0;
 }
